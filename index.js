f73317a690ac329876d6665f4115f647fbaf5ba4
Alex <alexn400@gmail.com>
2023-06-13 12:29:48 +1200

    Add base eslint config

---
+++ index.js
@@ -0,0 +1,81 @@
+/**
+ * This is the base configuration for all EasyRent Typescript projects.
+ * It should be preferred to use one of the other specific configurations for
+ * a project (such as the React or Node configurations).
+ */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'unused-imports',
+    'import-helpers',
+    "import"
+  ],
+  root: true,
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      typescript: true,
+      node: true
+    }
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+
+    'line-comment-position': [
+      'error',
+      {
+        position: 'above',
+      },
+    ],
+    'spaced-comment': ['error', 'always'],
+    'no-trailing-spaces': [
+      'error',
+      {
+        skipBlankLines: true,
+      },
+    ],
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'import/newline-after-import': 'error',
+    "import/no-unresolved": 'error',
+    'import/no-cycle': 'error',
+    'import-helpers/order-imports': [
+      'error',
+      {
+        newlinesBetween: 'always',
+        groups: ['module'],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+    // Unused imports handling
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+};
